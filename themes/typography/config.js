5120da06a3460a1ed2990add1c12925c26dd2c4a
dzu <h1754141938@163.com>
2025-07-20 14:00:44 +0800

    Update config.js

--- themes/typography/config.js
+++ themes/typography/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // 博客標題 雙語言
   TYPOGRAPHY_BLOG_NAME: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || '🐑の小屋',
-  TYPOGRAPHY_BLOG_NAME_EN: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || '🐑's little house',
+  TYPOGRAPHY_BLOG_NAME_EN: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || '🐑のlittle house',
 
   TYPOGRAPHY_POST_AD_ENABLE: process.env.NEXT_PUBLIC_TYPOGRAPHY_POST_AD_ENABLE || false, // 文章列表是否插入广告
 
